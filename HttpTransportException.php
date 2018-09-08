c977572c522234ef5f15803a723b20a93fae4175
Ramzi Alqrainy <ramzi.alqrainy@gmail.com>
2018-09-08 22:07:47 +0300

    Update HttpTransportException.php

--- HttpTransportException.php
+++ HttpTransportException.php
@@ -1,6 +1,7 @@
 <?php
+namespace ramzialqrainy\solr;
 /**
- * Copyright (c) 2007-2011, Servigistics, Inc.
+ * Copyright (c) 2007-2018, Servigistics, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,11 +30,11 @@
  *
  * @copyright Copyright 2007-2011 Servigistics, Inc. (http://servigistics.com)
  * @license http://solr-php-client.googlecode.com/svn/trunk/COPYING New BSD
- * @version $Id: HttpTransportException.php 54 2011-02-04 16:29:18Z donovan.jimenez $
+ * @version $Id: HttpTransportException.php 
  *
  * @package Apache
  * @subpackage Solr
- * @author Donovan Jimenez <djimenez@conduit-it.com>
+ * @author Ramzi Alqrainy <ramzi.alqrainy@gmail.com>
  */
 
 class Apache_Solr_HttpTransportException extends Apache_Solr_Exception
